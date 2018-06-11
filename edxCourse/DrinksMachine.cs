ae87661b5960cba8dd586e4535e2b562294b7845
Michael DAntuono <mikeishere3@gmail.com>
2018-06-11 17:22:19 +0000

    edx file added

---
+++ edxCourse/DrinksMachine.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrinksMachine myMachine = new DrinksMachine("Kitchen", "Brand", "DM1000");
+            //myMachine.Location = "Kitchen";
+            //myMachine.Model = "DM1000";
+
+            Console.WriteLine(myMachine.Location);
+            Console.WriteLine(myMachine.Make);
+            Console.WriteLine(myMachine.Model);
+
+            myMachine.MakeCappuccino();
+        }
+    }
+
+    class DrinksMachine
+    {
+        public DrinksMachine(string loc, string make, string model)
+        {
+            this.Location = loc;
+            this.Make = make;
+            this.Model = model;
+        }
+
+        private string _location;
+        private string _make;
+        private string _model;
+
+        public string Location { get => _location; set => _location = value; }
+        public string Make { get => _make; set => _make = value; }
+        public string Model { get => _model; set => _model = value; }
+
+        public void MakeCappuccino()
+        {
+            Console.WriteLine("Cappuccino made");
+        }
+    }
+}
