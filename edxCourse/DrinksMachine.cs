7cacde4f65a8b0801fad490c52e5f7149381520d
Michael DAntuono <mikeishere3@gmail.com>
2018-06-13 20:09:28 +0000

    edx course modlab1 done

--- edxCourse/DrinksMachine.cs
+++ edxCourse/DrinksMachine.cs
@@ -7,8 +7,8 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             DrinksMachine myMachine = new DrinksMachine("Kitchen", "Brand", "DM1000");
-            //myMachine.Location = "Kitchen";
-            //myMachine.Model = "DM1000";
+            // myMachine.Location = "Kitchen";
+            // myMachine.Model = "DM1000";
 
             Console.WriteLine(myMachine.Location);
             Console.WriteLine(myMachine.Make);
