d0267721f5ca31da7d92ee4fd556ee91465ba422
Michael DAntuono <mikeishere3@gmail.com>
2018-06-23 17:06:13 +0000

    started mosh course

---
+++ moshCourse/sample.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Mike";
+            bool isWorking = true;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+        }
+    }
+}
